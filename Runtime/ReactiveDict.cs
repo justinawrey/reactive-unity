b230e9dfe72c9652bfd32b802ae87b8bd420c97c
Justin Awrey <awreyjustin@gmail.com>
2023-11-16 18:59:50 +0200

    add ReactiveDict

---
+++ Runtime/ReactiveDict.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+namespace ReactiveUnity
+{
+  public class ReactiveDict<K, V> : Dictionary<K, V>
+  {
+    private List<Action<K, V>> _addCbs = new List<Action<K, V>>();
+    private List<Action<K, V>> _removeCbs = new List<Action<K, V>>();
+
+    public void OnAdd(Action<K, V> cb)
+    {
+      _addCbs.Add(cb);
+    }
+
+    public void OnRemove(Action<K, V> cb)
+    {
+      _removeCbs.Add(cb);
+    }
+
+    public new void Add(K key, V val)
+    {
+      base.Add(key, val);
+
+      foreach (Action<K, V> cb in _addCbs)
+      {
+        cb(key, val);
+      }
+    }
+
+    public new void Remove(K key)
+    {
+      V val = this[key];
+      base.Remove(key);
+
+      foreach (Action<K, V> cb in _removeCbs)
+      {
+        cb(key, val);
+      }
+    }
+  }
+}
