5848492978aa2fa822088a1cdf3b0d730e4f7cb1
coalstevens <62672483+coalstevens@users.noreply.github.com>
2023-11-30 16:06:13 +0300

    Add nullability to ref types; removal of all cbs now removes dict entry

--- Runtime/Reactive.cs
+++ Runtime/Reactive.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 namespace ReactiveUnity
 {
     [Serializable]
+    #nullable enable
     public class Reactive<T>
     {
-        [SerializeField] private T _val;
-        private Dictionary<Func<T, T, bool>, List<Action<T, T>>> _predicates = new Dictionary<Func<T, T, bool>, List<Action<T, T>>>();
+        [SerializeField] private T? _val;
+        private Dictionary<Func<T?, T?, bool>, List<Action<T?, T?>>> _predicates = new Dictionary<Func<T?, T?, bool>, List<Action<T?, T?>>>();
 
-        public Reactive(T val)
+        public Reactive(T? val)
         {
             _val = val;
         }
 
         // Convenience getter & setter
-        public T Value
+        public T? Value
         {
             get
             {
@@ -28,33 +29,40 @@ namespace ReactiveUnity
             }
         }
 
-        public Action When(Func<T, T, bool> predicate, Action<T, T> cb)
+        public Action When(Func<T?, T?, bool> predicate, Action<T?, T?> cb)
         {
             AddCb(predicate, cb);
             return () => RemoveCb(predicate, cb);
         }
 
-        public Action OnChange(Action<T, T> cb)
+        public Action OnChange(Action<T?, T?> cb)
         {
-            Func<T, T, bool> predicate = (_, __) => true;
+            Func<T?, T?, bool> predicate = (_, __) => true;
             AddCb(predicate, cb);
             return () => RemoveCb(predicate, cb);
         }
 
-        private void Set(T to)
+        private void Set(T? to)
         {
-            T prevValue = _val;
+            T? prevValue = _val;
             _val = to;
+            
+            if (prevValue != null) {
+                if (prevValue.Equals(_val))
+                {
+                    return;
+                }
+            }
 
-            if (prevValue.Equals(_val))
+            if (prevValue == null & _val == null) 
             {
                 return;
             }
 
             foreach (var pair in _predicates)
             {
-                Func<T, T, bool> predicate = pair.Key;
-                List<Action<T, T>> cbs = pair.Value;
+                Func<T?, T?, bool> predicate = pair.Key;
+                List<Action<T?, T?>> cbs = pair.Value;
 
                 if (!predicate(prevValue, _val))
                 {
@@ -65,7 +73,7 @@ namespace ReactiveUnity
             }
         }
 
-        private void AddCb(Func<T, T, bool> predicate, Action<T, T> cb)
+        private void AddCb(Func<T?, T?, bool> predicate, Action<T?, T?> cb)
         {
             if (_predicates.ContainsKey(predicate))
             {
@@ -73,18 +81,25 @@ namespace ReactiveUnity
             }
             else
             {
-                _predicates[predicate] = new List<Action<T, T>>() { cb };
+                _predicates[predicate] = new List<Action<T?, T?>>() { cb };
             }
         }
 
-        private void RemoveCb(Func<T, T, bool> predicate, Action<T, T> cb)
+        private void RemoveCb(Func<T?, T?, bool> predicate, Action<T?, T?> cb)
         {
             if (!_predicates.ContainsKey(predicate))
             {
                 return;
             }
-
+            
+            if(_predicates[predicate].Count == 1) 
+            {
+                _predicates.Remove(predicate);
+                return;
+            }
+            
             _predicates[predicate].Remove(cb);
         }
     }
+    #nullable disable
 }
