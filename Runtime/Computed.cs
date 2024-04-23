e86a53260f4b5a5587272daa7c02e603aabe20e2
Justin Awrey <awreyjustin@gmail.com>
2024-04-23 08:05:44 +0200

    more ergonomic comptued variable

--- Runtime/Computed.cs
+++ Runtime/Computed.cs
@@ -1,123 +1,107 @@
 using System;
+using UnityEngine;
 
 namespace ReactiveUnity
 {
-  [Serializable]
-  public class Computed<TOut, TIn> : ReactiveBase<TOut>
-  {
-    private readonly IReactiveCallbackOwner<TIn> _tracked;
-
-    public Computed(IReactiveCallbackOwner<TIn> tracked)
+    [Serializable]
+    public class Computed<TOut> : ReactiveBase<TOut>
     {
-      _tracked = tracked;
+        private Action _unsub = null;
+
+        public Action React<TIn1>(IReactiveCallbackOwner<TIn1> tracked, Func<TIn1, TOut> cb)
+        {
+            Set(cb(tracked.Value));
+            return CacheUnsubCb(tracked.OnChange(curr => Set(cb(curr))));
+        }
+
+        private Action CacheUnsubCb(Action unsub)
+        {
+            if (_unsub != null)
+            {
+                _unsub();
+                Debug.LogWarning(
+                    "reactive-unity (computed): computed variable unsubscribe hooks have been overwritten.  Did you call 'React' twice?"
+                );
+            }
+            _unsub = unsub;
+            return _unsub;
+        }
+
+        public Action React<TIn1, TIn2>(
+            IReactiveCallbackOwner<TIn1> tracked1,
+            IReactiveCallbackOwner<TIn2> tracked2,
+            Func<TIn1, TIn2, TOut> cb
+        )
+        {
+            Set(cb(tracked1.Value, tracked2.Value));
+
+            Action unsub1 = tracked1.OnChange(curr => Set(cb(curr, tracked2.Value)));
+            Action unsub2 = tracked2.OnChange(curr => Set(cb(tracked1.Value, curr)));
+
+            return CacheUnsubCb(() =>
+            {
+                unsub1();
+                unsub2();
+            });
+        }
+
+        public Action React<TIn1, TIn2, TIn3>(
+            IReactiveCallbackOwner<TIn1> tracked1,
+            IReactiveCallbackOwner<TIn2> tracked2,
+            IReactiveCallbackOwner<TIn3> tracked3,
+            Func<TIn1, TIn2, TIn3, TOut> cb
+        )
+        {
+            Set(cb(tracked1.Value, tracked2.Value, tracked3.Value));
+
+            Action unsub1 = tracked1.OnChange(curr =>
+                Set(cb(curr, tracked2.Value, tracked3.Value))
+            );
+            Action unsub2 = tracked2.OnChange(curr =>
+                Set(cb(tracked1.Value, curr, tracked3.Value))
+            );
+            Action unsub3 = tracked3.OnChange(curr =>
+                Set(cb(tracked1.Value, tracked2.Value, curr))
+            );
+
+            return CacheUnsubCb(() =>
+            {
+                unsub1();
+                unsub2();
+                unsub3();
+            });
+        }
+
+        public Action React<TIn1, TIn2, TIn3, TIn4>(
+            IReactiveCallbackOwner<TIn1> tracked1,
+            IReactiveCallbackOwner<TIn2> tracked2,
+            IReactiveCallbackOwner<TIn3> tracked3,
+            IReactiveCallbackOwner<TIn4> tracked4,
+            Func<TIn1, TIn2, TIn3, TIn4, TOut> cb
+        )
+        {
+            Set(cb(tracked1.Value, tracked2.Value, tracked3.Value, tracked4.Value));
+
+            Action unsub1 = tracked1.OnChange(curr =>
+                Set(cb(curr, tracked2.Value, tracked3.Value, tracked4.Value))
+            );
+            Action unsub2 = tracked2.OnChange(curr =>
+                Set(cb(tracked1.Value, curr, tracked3.Value, tracked4.Value))
+            );
+            Action unsub3 = tracked3.OnChange(curr =>
+                Set(cb(tracked1.Value, tracked2.Value, curr, tracked4.Value))
+            );
+            Action unsub4 = tracked4.OnChange(curr =>
+                Set(cb(tracked1.Value, tracked2.Value, tracked3.Value, curr))
+            );
+
+            return CacheUnsubCb(() =>
+            {
+                unsub1();
+                unsub2();
+                unsub3();
+                unsub4();
+            });
+        }
     }
-
-    public Action React(Func<TIn, TOut> cb)
-    {
-      Set(cb(_tracked.Value));
-      return _tracked.OnChange(curr => Set(cb(curr)));
-    }
-  }
-
-  [Serializable]
-  public class Computed<TOut, TIn1, TIn2> : ReactiveBase<TOut>
-  {
-    private readonly IReactiveCallbackOwner<TIn1> _tracked1;
-    private readonly IReactiveCallbackOwner<TIn2> _tracked2;
-
-    public Computed(IReactiveCallbackOwner<TIn1> tracked1, IReactiveCallbackOwner<TIn2> tracked2)
-    {
-      _tracked1 = tracked1;
-      _tracked2 = tracked2;
-    }
-
-    public Action React(Func<TIn1, TIn2, TOut> cb)
-    {
-      Set(cb(_tracked1.Value, _tracked2.Value));
-
-      Action unsub1 = _tracked1.OnChange(curr => Set(cb(curr, _tracked2.Value)));
-      Action unsub2 = _tracked2.OnChange(curr => Set(cb(_tracked1.Value, curr)));
-
-      return () =>
-      {
-        unsub1();
-        unsub2();
-      };
-    }
-  }
-
-  [Serializable]
-  public class Computed<TOut, TIn1, TIn2, TIn3> : ReactiveBase<TOut>
-  {
-    private readonly IReactiveCallbackOwner<TIn1> _tracked1;
-    private readonly IReactiveCallbackOwner<TIn2> _tracked2;
-    private readonly IReactiveCallbackOwner<TIn3> _tracked3;
-
-    public Computed(
-      IReactiveCallbackOwner<TIn1> tracked1,
-      IReactiveCallbackOwner<TIn2> tracked2,
-      IReactiveCallbackOwner<TIn3> tracked3
-    )
-    {
-      _tracked1 = tracked1;
-      _tracked2 = tracked2;
-      _tracked3 = tracked3;
-    }
-
-    public Action React(Func<TIn1, TIn2, TIn3, TOut> cb)
-    {
-      Set(cb(_tracked1.Value, _tracked2.Value, _tracked3.Value));
-
-      Action unsub1 = _tracked1.OnChange(curr => Set(cb(curr, _tracked2.Value, _tracked3.Value)));
-      Action unsub2 = _tracked2.OnChange(curr => Set(cb(_tracked1.Value, curr, _tracked3.Value)));
-      Action unsub3 = _tracked3.OnChange(curr => Set(cb(_tracked1.Value, _tracked2.Value, curr)));
-
-      return () =>
-      {
-        unsub1();
-        unsub2();
-        unsub3();
-      };
-    }
-  }
-
-  [Serializable]
-  public class Computed<TOut, TIn1, TIn2, TIn3, TIn4> : ReactiveBase<TOut>
-  {
-    private readonly IReactiveCallbackOwner<TIn1> _tracked1;
-    private readonly IReactiveCallbackOwner<TIn2> _tracked2;
-    private readonly IReactiveCallbackOwner<TIn3> _tracked3;
-    private readonly IReactiveCallbackOwner<TIn4> _tracked4;
-
-    public Computed(
-      IReactiveCallbackOwner<TIn1> tracked1,
-      IReactiveCallbackOwner<TIn2> tracked2,
-      IReactiveCallbackOwner<TIn3> tracked3,
-      IReactiveCallbackOwner<TIn4> tracked4
-    )
-    {
-      _tracked1 = tracked1;
-      _tracked2 = tracked2;
-      _tracked3 = tracked3;
-      _tracked4 = tracked4;
-    }
-
-    public Action React(Func<TIn1, TIn2, TIn3, TIn4, TOut> cb)
-    {
-      Set(cb(_tracked1.Value, _tracked2.Value, _tracked3.Value, _tracked4.Value));
-
-      Action unsub1 = _tracked1.OnChange(curr => Set(cb(curr, _tracked2.Value, _tracked3.Value, _tracked4.Value)));
-      Action unsub2 = _tracked2.OnChange(curr => Set(cb(_tracked1.Value, curr, _tracked3.Value, _tracked4.Value)));
-      Action unsub3 = _tracked3.OnChange(curr => Set(cb(_tracked1.Value, _tracked2.Value, curr, _tracked4.Value)));
-      Action unsub4 = _tracked4.OnChange(curr => Set(cb(_tracked1.Value, _tracked2.Value, _tracked3.Value, curr)));
-
-      return () =>
-      {
-        unsub1();
-        unsub2();
-        unsub3();
-        unsub4();
-      };
-    }
-  }
 }
